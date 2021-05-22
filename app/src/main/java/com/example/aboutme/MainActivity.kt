820df58ade947f0346a1e43d5922a226ee174c60
VojMi <vojtech.miklas@gmail.com>
2021-05-22 10:00:39 +0200

    Initial commit of Udacity 'About me' lesson

---
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.aboutme
+
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.nicknameButton).setOnClickListener {
+            addNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View) {
+        val editText = findViewById<EditText>(R.id.text_nickname)
+        val nicknameTextView = findViewById<TextView>(R.id.nickName)
+
+        nicknameTextView.text=editText.text
+        editText.visibility=View.GONE
+        nicknameTextView.visibility=View.VISIBLE
+
+        // Hide the keyboard.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+}
